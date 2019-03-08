0c74e15d93984b6a5e4e028329a82dc60b02dc91
TuxedoFish <harryliversedge@gmail.com>
2019-03-08 22:21:19 +0000

    Aesthetic changes

--- components/Tutorial.jsx
+++ components/Tutorial.jsx
@@ -9,13 +9,17 @@ class Tutorial extends React.Component {
 	render() {
 	    return <Card className="tutorial-card">
                 <CardContent>
-                  <Typography variant ="h1" className="tutorial-title">
+                  <Typography variant ="h1" className="title">
                     {"Let's write some code!"}
+                  </Typography>
+                  <Typography variant ="h5" className="text">
+                    Congratulations on getting this far if people are still struggling: help them out or 
+                    take a look at the code in 'components'
                   </Typography>
 
                
 
-               
+
 
                 </CardContent>
                </Card>;
